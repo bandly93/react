cecc67a85d2d64b7f6270b0ef4235ef725e2e9d7
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-05-30 14:59:20 -0700

    inital repo

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 
-//functional component
-//****************
-// const SearchBar = () => {
-// 	return <input />;
-// };
-// ***************
 
 //gives our searchbar a bunch of function from the react functionality. 
 class SearchBar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {term: ''}
+	}
 	render() {
-		return <input />;
-	};
-};
-
-
-
-
-
+		return (
+			<div>
+				<input 
+					value = {this.state.term}
+					onChange={event => this.setState({ term: event.target.value})}
+				/>
+			</div>
+		);
+	}
+}
 export default SearchBar;
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyBINItlDqo69U9Gm4cVXLann_2drblkRKw';
+const API_KEY = 'AIzaSyCh7oq6a5gHJAdQ1sj7KCsxar_taqZTXPc';
 
 //Create a new component. This should produce some HTML.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = {videos: []};
+		
+		YTSearch({key: API_KEY, term:'lakers'},(videos) => {
+			this.setState = ({videos});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos = {this.state.videos}/>
+			</div>
+		);
+	}
+}
 
 //Take this component's generated HTML and put it on the page.
-
-
 ReactDOM.render(<App />, document.querySelector('.container'));
