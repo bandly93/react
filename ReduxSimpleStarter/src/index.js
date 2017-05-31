f9005b9cd1656aa83cc8bb88eedbaaf9efb7ece6
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-05-31 13:38:23 -0700

    fixing repo

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,28 +1,42 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCh7oq6a5gHJAdQ1sj7KCsxar_taqZTXPc';
 
 //Create a new component. This should produce some HTML.
 class App extends Component{
-
 	constructor(props){
 		super(props);
-		this.state = {videos: []};
-		
-		YTSearch({key: API_KEY, term:'lakers'},(videos) => {
-			this.setState = ({videos});
+
+		this.state = {
+			videos:[],
+			selectedVideo:null
+		};
+
+		this.videoSearch('warriors')
+	}
+	videoSearch(term){
+		YTSearch({key: API_KEY, term:term},(videos) => {
+			this.setState({
+				videos:videos,
+				selectedVideo:videos[0]
+			});
 		});
 	}
-
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos = {this.state.videos}/>
+				<SearchBar onSearchTermChange = {videoSearch}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
@@ -30,3 +44,6 @@ class App extends Component{
 
 //Take this component's generated HTML and put it on the page.
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
